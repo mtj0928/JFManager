b2215e2f873854d23306501b36857ee051f6bf93
mtj0928 <mtj0928@users.noreply.github.com>
2018-11-30 20:52:08 +0900

    first commit.

---
+++ JFManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JFManager
+//
+//  Created by 松本淳之介 on 2018/11/30.
+//  Copyright © 2018 Kusumoto Labn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
